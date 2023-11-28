ea59c3739b336df721440e8161eff5843f1a3494
andpramor <andpramordev@gmail.com>
2023-11-28 08:46:19 +0100

    Así estaba el último día

--- libreria/models.py
+++ libreria/models.py
@@ -1,50 +1,69 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.core import validators
 
 GENEROS = (
-    ('Fantasía'),
-    ('No Ficción'),
-    ('Terror'),
-    ('Ciencia Ficción'),
+    ('fantasia', 'Fantasía'),
+    ('no-ficcion', 'No Ficción'),
+    ('terror', 'Terror'),
+    ('ciencia-ficcion', 'Ciencia Ficción'),
 )
 
 DISPONIBILIDAD_VALORES = (
-    ('Disponible'),
-    ('Prestado'),
-    ('En proceso de préstamo'),
+    ('disponible', 'Disponible'),
+    ('prestado', 'Prestado'),
+    ('en-proceso', 'En proceso de préstamo'),
 )
 
-#class Usuario(models.Model): #Hacer este con AbstractUser
-    #atributos
-    #metodos
-
-class Libro(models.Model):
-    titulo = models.CharField(max_length=50)
-    #autores
-    #editorial
-    fecha_publicacion = models.DateField()
-    genero = models.CharField(max_length=20, choices=GENEROS)
-    ISBN = models.BigIntegerField(validators=[models.validators.MinValueValidator(1000000000000), models.validators.MaxValueValidator(9999999999999)]) #El ISBN es un número de 13 cifras y max_length no se le aplica.
-    resumen = models.TextField(max_length=500)
-    disponibilidad = models.CharField(max_length=30, choices=DISPONIBILIDAD_VALORES)
-    portada = models.ImageField()
+class Usuario(AbstractUser):
+    dni = models.CharField(max_length=9)
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    telefono = models.BigIntegerField(validators=[validators.MinValueValidator(100000000), validators.MaxValueValidator(999999999)])
 
     def __str__(self):
-        return self.titulo
+        return self.nombre
 
 class Autor(models.Model):
     nombre = models.CharField(max_length=50)
     biografia = models.TextField(max_length=500)
-    foto = models.ImageField()
+    foto = models.ImageField(upload_to='autores/', null=True, blank=True)
 
-class Editorial:
+    def __str__(self):
+        return self.nombre
+
+class Editorial(models.Model):
     nombre = models.CharField(max_length=50)
     direccion = models.CharField(max_length=100)
-    sitio_web = models.CharField(max_length=100)
+    sitio_web = models.URLField()
+
+    def __str__(self):
+        return self.nombre
+    
+class Libro(models.Model):
+    titulo = models.CharField(max_length=50)
+    autores = models.ManyToManyField(Autor)
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
+    fecha_publicacion = models.DateField()
+    genero = models.CharField(max_length=20, choices=GENEROS)
+    ISBN = models.BigIntegerField(validators=[validators.MinValueValidator(1000000000000), validators.MaxValueValidator(9999999999999)]) #El ISBN es un número de 13 cifras.
+    resumen = models.TextField(max_length=500)
+    disponibilidad = models.CharField(max_length=30, choices=DISPONIBILIDAD_VALORES)
+    portada = models.ImageField(upload_to='portadas/', null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo
 
 class Préstamo:
-    #libro_prestado
+    ESTADO_CHOICES = (
+        ('prestado','Prestado'),
+        ('devuelto','Devuelto',)
+    )
+    libro_prestado = models.ForeignKey(Libro, on_delete=models.SET_NULL)
     fecha_prestamo = models.DateField()
-    fecha_devolucion = models.DateField()
-    #usuario
-    estado_prestamo = models.CharField(max_length=20, choices=('Prestado','Devuelto',))
+    fecha_devolucion = models.DateField(null=True,blank=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL)
+    estado_prestamo = models.CharField(max_length=20, choices=ESTADO_CHOICES)
+
+    def __str__(self):
+        return self.usuario
--- libreriaAndres/settings.py
+++ libreriaAndres/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +27,8 @@ SECRET_KEY = 'django-insecure-o01#%o2wx#u=p0*9*pqr36-^-y^48jnderu9su1$td@cy5pb@%
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+AUTH_USER_MODERL = "libreria.Usuario"
 
 
 # Application definition
@@ -104,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
@@ -117,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/ 'static'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
